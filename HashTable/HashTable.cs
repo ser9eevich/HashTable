f5ba8fb52e3132ff5c7e6f9b731133590f8bb7ff
ser9eevich <p.danya@mail.ru>
2019-03-28 21:22:37 +0500

    Initial commit

---
+++ HashTable/HashTable.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    class HashItem
+    {
+        public int HashCode;
+        public object Value;
+        public HashItem(int hash, object value)
+        {
+            HashCode = hash;
+            Value = value;
+        }
+    }
+    class HashTable
+    {
+        private List<HashItem>[] HashInfo;
+
+        public HashTable(int size)
+        {
+            HashInfo = new List<HashItem>[size];
+        }
+
+        public void PutPair(object key, object value)
+        {
+            var hashCode = key.GetHashCode();
+            int index;
+
+            if (hashCode < 0)
+                index = (-hashCode) % HashInfo.Length;
+            else
+                index = hashCode % HashInfo.Length;
+
+            if (HashInfo[index] == null)
+                HashInfo[index] = new List<HashItem> { new HashItem(hashCode, value) };
+            else
+            {
+                var element = HashInfo[index];
+                if (element.FirstOrDefault(e => e.HashCode == hashCode) == null)
+                    HashInfo[index].Add(new HashItem(hashCode, value));
+                else
+                    element.FirstOrDefault(e => e.HashCode == hashCode).Value = value;
+            }
+        }
+
+        public object GetValueByKey(object key)
+        {
+            var hashCode = key.GetHashCode();
+            int index;
+
+            if (hashCode < 0)
+                index = (-hashCode) % HashInfo.Length;
+            else
+                index = hashCode % HashInfo.Length;
+
+            if (HashInfo[index] == null)
+                return null;
+
+            foreach (var list in HashInfo[index])
+            {
+                if (list.HashCode == hashCode)
+                    return list.Value;
+            }
+            return null;
+        }
+    }
+}
---
+++ HashTable/HashTableTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace HashTable
+{
+    [TestFixture]
+    class HashTableTests
+    {
+        [Test]
+        public void ThreeElements()
+        {
+            var hashTable = new HashTable(3);
+            var arr = new int[] { 1, 2, 3 };
+
+            hashTable.PutPair(1, "message");
+            hashTable.PutPair("key", 'c');
+            hashTable.PutPair(228, arr);
+
+            Assert.AreEqual("message", hashTable.GetValueByKey(1));
+            Assert.AreEqual('c', hashTable.GetValueByKey("key"));
+            Assert.AreEqual(arr, hashTable.GetValueByKey(228));
+        }
+        [Test]
+        public void SameElements()
+        {
+            var hashTable = new HashTable(2);
+
+            hashTable.PutPair("super key", "message");
+            hashTable.PutPair("super key", "second message");
+
+            Assert.AreEqual("second message", hashTable.GetValueByKey("super key"));
+        }
+        [Test]
+        public void TenThousandsElements()
+        {
+            var hashTable = new HashTable(10000);
+            var rnd = new Random();
+            var randomValue = rnd.Next();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                if (i == 578) //взял случайное i
+                    hashTable.PutPair("" + i, randomValue);
+                else
+                    hashTable.PutPair("" + i, rnd.Next());
+            }
+
+            Assert.AreEqual(randomValue, hashTable.GetValueByKey("578"));
+        }
+        [Test]
+        public void TenThousandsElementsAndOtherKeys()
+        {
+            var hashTable = new HashTable(10000);
+            var rnd = new Random();
+            var randomValue = rnd.Next();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                hashTable.PutPair("" + i, rnd.Next());
+            }
+
+            for (int i = 0; i < 1000; i++)
+                Assert.AreEqual(null, hashTable.GetValueByKey(i));
+        }
+    }
+}
